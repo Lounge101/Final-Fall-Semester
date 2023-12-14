18e7092275bfa046f48b85ac7374d55b1f3ac53e
Lounge101 <noahtoledo170@riversideunified.org>
2023-12-14 14:21:44 -0800

    Working on milestone 3

--- Mini Game/Assets/Scripts/UIManager.cs
+++ Mini Game/Assets/Scripts/UIManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public TextMeshProUGUI coinAmountText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinAmountText.text = "Coint: 0";
     }
 
     // Update is called once per frame
